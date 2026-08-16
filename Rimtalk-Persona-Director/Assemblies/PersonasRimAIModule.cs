1462bc81c4b48daf7e7d3ba59ad06ddd456c73ca
Dmytro Vostrikov <ustas@barco.kiev.ua>
2026-08-15 23:00:34 -0700

    Stage 7G: register Personas in the RimAI module catalog.

---
+++ Rimtalk-Persona-Director/Assemblies/PersonasRimAIModule.cs
@@ -0,0 +1,19 @@
+using Ustas.RimAI.Core.Modules;
+using Verse;
+
+namespace RimPersonaDirector
+{
+    [StaticConstructorOnStartup]
+    public static class PersonasRimAIModule
+    {
+        static PersonasRimAIModule()
+        {
+            RimAIModuleRegistry.Current.Register(new RimAIModuleDescriptor(
+                "personas",
+                "RimAI.Communication.Personas",
+                "RimAI.Communication.Personas",
+                "Communication",
+                "RimAI.Communication"));
+        }
+    }
+}
